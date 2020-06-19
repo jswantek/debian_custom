c65ad9439bfa017e5b975b7e89ed88c4fc7703c4
Joe Swantek <joseph.swantek@northwestern.edu>
2020-06-19 12:59:57 -0500

    Build 9 (stretch) and 10 (buster) custom images & add lsb-release

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-#!
+#!/bin/bash
 
-docker build . --pull -f docker/Dockerfile \
-    -t debian:stretch_custom
+docker build . --pull -f docker/Dockerfile.9.stretch \
+    -t debian:9.stretch_custom
+
+docker build . --pull -f docker/Dockerfile.10.buster \
+    -t debian:10.buster_custom
